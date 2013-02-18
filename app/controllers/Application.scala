6bf027032a87ccec988b1ba80a2ad7c0df7dd208
Lindsey Dew <lindsey.dew@guardian.co.uk>
2013-02-18 11:44:15 +0000

    add a redirect to list of guardian urls

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
 
 import play.api._
-import play.api.mvc._
+import mvc._
+import models.Urls._
 
 object Application extends Controller {
-  
+
+  val urls = Urls.urls
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(urls))
   }
-  
+
+  def test1 = Action {
+    val url = Urls.urls("/test1")
+    Redirect(url)
+  }
+
+
 }
